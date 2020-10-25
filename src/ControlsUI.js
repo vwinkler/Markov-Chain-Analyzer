4f9ebe33b15a59fce612dcdf818ce3e4e6ab0b15
Vincent Winkler <vincent.winkler@student.kit.edu>
2020-10-25 17:58:58 +0100

    Add permalink age display

--- src/ControlsUI.js
+++ src/ControlsUI.js
@@ -1,6 +1,35 @@
 class ControlsUI {
     constructor(graph) {
         this.graph = graph;
+        this.lastUpdateDate = ControlsUI.addSecondsToDate(-60, new Date(Date.now()));
+        this.enablePermalinkAgeDisplay();
+    }
+
+    static addSecondsToDate(seconds, date) {
+        let result = new Date(date);
+        result.setSeconds(result.getSeconds() + seconds);
+        return result;
+    }
+
+    enablePermalinkAgeDisplay() {
+        let that = this;
+        let updateAge = function () {
+            let elapsedSeconds = (new Date(Date.now()) - that.lastUpdateDate) / 1000;
+            let timeText = ControlsUI.formatPermalinkAge(elapsedSeconds);
+            document.getElementById("permalinkAge").innerHTML = "" + timeText + "s";
+        };
+        updateAge();
+        setInterval(updateAge, 100);
+    }
+
+    static formatPermalinkAge(elapsedSeconds) {
+        let timeText;
+        if (elapsedSeconds <= 60) {
+            timeText = "" + Math.round(elapsedSeconds / 10) * 10;
+        } else {
+            timeText = "> 60";
+        }
+        return timeText;
     }
 
     displayControls() {
@@ -11,6 +40,7 @@ class ControlsUI {
     displayPermalink() {
         let url = this.makePermanentUrl(this.graph);
         document.getElementById("permalink").href = url.toString();
+        this.lastUpdateDate = new Date(Date.now());
     }
 
     displayBugreportLink() {
